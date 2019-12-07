ed430685ef7df2c092cd5d47b0ffc09613036074
worepix <hry-kurasa@seznam.cz>
2019-12-07 23:19:15 +0100

    Add backend functionality

--- backend.py
+++ backend.py
@@ -1,11 +1,16 @@
 import sys
+import click
 from firebase import Firebase
 from flask import Flask
 from flask import request
-from flask_api import status
+from flask import make_response
 import requests
 
-def main():
+
+@click.command()
+@click.option('--hostname', '-h', required=True)
+@click.option('--port', '-p', required=True)
+def main(hostname=None, port=None):
     config = {
         "apiKey": "AIzaSyD9kyPxezXpH7mhRwWULwhrehEI-LaZjzY",
         "databaseURL": "https://smart-amplifier-gsyadn.firebaseio.com/",
@@ -17,26 +22,89 @@ def main():
 
     app = Flask(__name__)
 
-    @app.route("/get/paired/amplifier/<email>", methods=['GET'])
+    @app.route('/get/paired/amplifier/<email>', methods=['GET'])
     def paired_amplifier(email):
         value = firebase_db.child('users').child(str(email).split(
-                '@')[0]).child('paired').child('amplifier').get()
+            '@')[0]).child('paired').get()
         return value.val()
 
-    @app.route("/pair/new/amplifier", methods=['POST'])
+    @app.route('/pair/new/amplifier', methods=['POST'])
     def paired_new_amplifier():
         try:
-            firebase_db.child('users').child(str(request.form['email']).split('@')[0]).child(
-            'paired').child('amplifier').set({'amplifier': request.form['amplifier']})
+            registred_amplifiers = firebase_db.child("amplifiers").child(
+                request.form['amplifier']).get().val()
 
-            return '200'
+            if registred_amplifiers == None:
+                return make_response({"Error": "Amplifier not registered"}, 400)
+
+            firebase_db.child('users').child(str(request.form['email']).split('@')[0]).child(
+                'paired').set({'amplifier': request.form['amplifier']})
+
+            return 'Sucesfully created', 200
 
         except requests.exceptions.HTTPError:
-            return '400'
+            return 'Error', 400
 
-    
-    
-    app.run('0.0.0.0', 8080)
+    @app.route('/register/new/amplifier', methods=['POST'])
+    def register_new_amplifier():
+        if firebase_db.child('amplifiers').child(request.form['amplifier']).set({"volume": 0}):
+            return 'Successfully registered', 200
+
+        return 'Successfully registered', 400
+
+    @app.route('/change/volume/by/id', methods=['POST'])
+    def change_volume_by_id():
+        amplifier = request.form['amplifier']
+        volume = request.form['volume']
+
+        if firebase_db.child('amplifiers').child(amplifier).child('volume').set(volume):
+            return 'Volume successfully changed', 200
+
+        return 'Error', 400
+
+    @app.route('/change/volume/by/email', methods=['POST'])
+    def change_volume_by_email():
+        try:
+            amplifier = firebase_db.child('users').child(
+                str(request.form['email']).split('@')[0]).child(
+                    'paired').child('amplifier').get().val()
+        except Exception:
+            return 'Account have no amplifiers paired', 400
+        volume = request.form['volume']
+
+        if firebase_db.child('amplifiers').child(amplifier).child('volume').set(volume):
+            return 'Volume successfully changed', 200
+
+        return 'Error', 400
+
+    @app.route('/get/volume/by/id/<id>', methods=['GET'])
+    def get_volume_by_id(id):
+        print(id)
+        volume = firebase_db.child('amplifiers').child(
+            id).child('volume').get().val()
+
+        if volume:
+            return {"volume": volume}, 200
+
+        return 'Error', 400
+
+    @app.route('/get/volume/by/email/<email>', methods=['GET'])
+    def get_volume_by_email(email):
+        try:
+            amplifier = firebase_db.child('users').child(
+                email.split('@')[0]).child(
+                    'paired').child('amplifier').get().val()
+        except Exception:
+            return 'Account have no amplifiers paired', 400
+
+        volume = firebase_db.child('amplifiers').child(
+            amplifier).child('volume').get().val()
+
+        if volume:
+            return {"volume": volume}, 200
+
+    app.run(hostname, port)
+
 
 if __name__ == '__main__':
     try:
---
+++ test.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import logging
+import random
+import requests
+
+
+def main():
+    # Set up logging
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)s: %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+    logging.root.setLevel(logging.NOTSET)
+
+    # Generate random TESTER-<INT> amplifier
+    tested_amplifier = 'TESTER-' + str(random.randint(111111, 999999))
+
+    # Try to pair generated amplifier before it is registred
+    logging.info(
+        'Try to pair virtual amplifier to account tester@email.xx')
+    pair_new_amplifier = requests.post('http://localhost:8080/pair/new/amplifier',
+                                       {"email": "tester@email.xx", "amplifier":
+                                        tested_amplifier})
+    if pair_new_amplifier.status_code != 200:
+        logging.info('Status code not 200 in pairing new amplifier. Code: {}'.format(
+            pair_new_amplifier.status_code))
+
+    else:
+        logging.critical('Virtual amplifier paired before it is created')
+        sys.exit(1)
+
+    # Register generated amplifier
+    logging.info('Register virtual amplifier')
+    register_new_amplifier = requests.post('http://localhost:8080/register/new/amplifier',
+                                           {"amplifier": tested_amplifier})
+    logging.info('Virtual amplifier successfully created')
+    if register_new_amplifier.status_code != 200:
+        logging.critical('Register failed')
+        sys.exit(1)
+
+    # Try to pair register generated virtual amplifier
+    logging.info(
+        'Try to pair virtual amplifier again after it is registered to account tester@email.xx')
+    pair_new_amplifier = requests.post('http://localhost:8080/pair/new/amplifier',
+                                       {"email": "tester@email.xx", "amplifier":
+                                        tested_amplifier})
+    if pair_new_amplifier.status_code != 200:
+        logging.info('Status code not 200. Code: {}'.format(
+            pair_new_amplifier.status_code))
+        sys.exit(1)
+
+    else:
+        logging.info('Virtual amplifier paired')
+
+    # Check if pair was successfull
+    logging.info('Check if virtual amplifier paired with account')
+    paired_amplifier = requests.get(
+        'http://localhost:8080/get/paired/amplifier/tester@email.xx').json()
+    if paired_amplifier['amplifier'] != str(tested_amplifier):
+        logging.critical(
+            'Paired amplifier is not one we paired in previous step')
+        sys.exit(1)
+
+    # Change volume to 40 by amplifier id
+    logging.info('Change volume to 40 by amplifier id')
+    change_volume = requests.post(
+        'http://localhost:8080/change/volume/by/id', {"amplifier": tested_amplifier, "volume": 40})
+    if change_volume.status_code != 200:
+        logging.critical('Error while changing volume')
+        sys.exit(1)
+
+    volume = requests.get(
+        'http://localhost:8080/get/volume/by/id/{}'.format(tested_amplifier)).json()['volume']
+
+    if volume == str(40):
+        logging.info('Change succesfull successfull')
+
+    else:
+        logging.critical('Volume not 40 after change')
+        sys.exit(1)
+
+    # Change volume to 80 by email
+    logging.info('Change volume to 40 by email')
+    change_volume = requests.post(
+        'http://localhost:8080/change/volume/by/email', {"email": "tester@email.xx", "volume": 80})
+    if change_volume.status_code != 200:
+        logging.critical('Error while changing volume')
+        sys.exit(1)
+
+    volume = requests.get(
+        'http://localhost:8080/get/volume/by/email/tester@email.xx').json()['volume']
+
+    if volume == str(80):
+        logging.info('Change succesfull successfull')
+
+    else:
+        logging.critical('Volume not 40 after change')
+        sys.exit(1)
+
+    # Now we are done, test successfull
+    logging.info('Test successfull')
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
+    except Exception as e:
+        print('Exited with error: ', e)
+        sys.exit(1)
