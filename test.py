96f3b8936df55ee02b3ac51849b0ed6102f07472
worepix <hry-kurasa@seznam.cz>
2019-12-08 12:11:16 +0100

    Change webserver port to 8345 on test

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ def main():
     # Run backend
     logging.info('Starting backend')
     subprocess.Popen(['python3', 'backend.py', '-h',
-                      'localhost', '-p', '8080'])
+                      'localhost', '-p', '8345'])
     time.sleep(2)
 
     # Generate random TESTER-<INT> amplifier
@@ -27,7 +27,7 @@ def main():
     # Try to pair generated amplifier before it is registred
     logging.info(
         'Try to pair virtual amplifier to account tester@email.xx')
-    pair_new_amplifier = requests.post('http://localhost:8080/pair/new/amplifier',
+    pair_new_amplifier = requests.post('http://localhost:8345/pair/new/amplifier',
                                        {"email": "tester@email.xx", "amplifier":
                                         tested_amplifier})
     if pair_new_amplifier.status_code != 200:
@@ -40,7 +40,7 @@ def main():
 
     # Register generated amplifier
     logging.info('Register virtual amplifier')
-    register_new_amplifier = requests.post('http://localhost:8080/register/new/amplifier',
+    register_new_amplifier = requests.post('http://localhost:8345/register/new/amplifier',
                                            {"amplifier": tested_amplifier})
     logging.info('Virtual amplifier successfully created')
     if register_new_amplifier.status_code != 200:
@@ -50,7 +50,7 @@ def main():
     # Try to pair register generated virtual amplifier
     logging.info(
         'Try to pair virtual amplifier again after it is registered to account tester@email.xx')
-    pair_new_amplifier = requests.post('http://localhost:8080/pair/new/amplifier',
+    pair_new_amplifier = requests.post('http://localhost:8345/pair/new/amplifier',
                                        {"email": "tester@email.xx", "amplifier":
                                         tested_amplifier})
     if pair_new_amplifier.status_code != 200:
@@ -64,7 +64,7 @@ def main():
     # Check if pair was successfull
     logging.info('Check if virtual amplifier paired with account')
     paired_amplifier = requests.get(
-        'http://localhost:8080/get/paired/amplifier/tester@email.xx').json()
+        'http://localhost:8345/get/paired/amplifier/tester@email.xx').json()
     if paired_amplifier['amplifier'] != str(tested_amplifier):
         logging.critical(
             'Paired amplifier is not one we paired in previous step')
@@ -73,13 +73,13 @@ def main():
     # Change volume to 40 by amplifier id
     logging.info('Change volume to 40 by amplifier id')
     change_volume = requests.post(
-        'http://localhost:8080/change/volume/by/id', {"amplifier": tested_amplifier, "volume": 40})
+        'http://localhost:8345/change/volume/by/id', {"amplifier": tested_amplifier, "volume": 40})
     if change_volume.status_code != 200:
         logging.critical('Error while changing volume')
         sys.exit(1)
 
     volume = requests.get(
-        'http://localhost:8080/get/volume/by/id/{}'.format(tested_amplifier)).json()['volume']
+        'http://localhost:8345/get/volume/by/id/{}'.format(tested_amplifier)).json()['volume']
 
     if volume == str(40):
         logging.info('Change succesfull successfull')
@@ -91,13 +91,13 @@ def main():
     # Change volume to 80 by email
     logging.info('Change volume to 40 by email')
     change_volume = requests.post(
-        'http://localhost:8080/change/volume/by/email', {"email": "tester@email.xx", "volume": 80})
+        'http://localhost:8345/change/volume/by/email', {"email": "tester@email.xx", "volume": 80})
     if change_volume.status_code != 200:
         logging.critical('Error while changing volume')
         sys.exit(1)
 
     volume = requests.get(
-        'http://localhost:8080/get/volume/by/email/tester@email.xx').json()['volume']
+        'http://localhost:8345/get/volume/by/email/tester@email.xx').json()['volume']
 
     if volume == str(80):
         logging.info('Change succesfull successfull')
